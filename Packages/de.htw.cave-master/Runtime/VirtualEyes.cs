fc8ac6cf5af4fd09abcf07e0bf5b583230bbc6be
Thorben Edvard Stumpe <s0573288@htw-berlin.de>
2022-08-30 23:26:58 +0200

    :bug: Workaround for the kinect-glasses issue

--- Packages/de.htw.cave-master/Runtime/VirtualEyes.cs
+++ Packages/de.htw.cave-master/Runtime/VirtualEyes.cs
@@ -7,41 +7,60 @@ using Htw.Cave.Kinect;
 
 namespace Htw.Cave
 {
-	/// <summary>
-	/// Responsible for transforming stereoscopic eyes from local to world space.
-	/// </summary>
-	[AddComponentMenu("Htw.Cave/Virtual Eyes")]
-	public sealed class VirtualEyes : MonoBehaviour
-	{
-		/// <summary>
-		/// Defines the eye of a stereo or mono output target.
-		/// </summary>
-		public enum StereoTarget
-		{
-			Mono,
-			Left,
-			Right
-		}
-	
-		/// <summary>
-		/// Returns the eye position based on the <see cref="StereoTarget"/>.
-		/// </summary>
-		/// <param name="stereoTarget">The target eye (stereo) or the eye anchor (mono).</param>
-		/// <param name="seperation">The distance between the eyes (ignored when using <see cref="StereoTarget.Mono"/>).</param>
-		/// <returns>The eye position in world space.</returns>
-		public Vector3 GetPosition(StereoTarget stereoTarget, float seperation = 0f)
-		{
-			switch(stereoTarget)
-			{
-				case StereoTarget.Mono:
-					return transform.position;
-				case StereoTarget.Left:
-					return transform.TransformPoint(Vector3.right * -seperation * 0.5f);
-				case StereoTarget.Right:
-					return transform.TransformPoint(Vector3.right * seperation * 0.5f);
-			}
-			
-			return Vector3.zero;
-		}	
-	}
+    /// <summary>
+    /// Responsible for transforming stereoscopic eyes from local to world space.
+    /// </summary>
+    [AddComponentMenu("Htw.Cave/Virtual Eyes")]
+    public sealed class VirtualEyes : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Workaround for the kinect not correctly detecting the players rotation due to the 3D-glasses covering the users eyes.
+        /// The patch will flip the virtual eye positions if an abnormal rotation is detected which *hopefully* prevents a breakdown of the stereoscopic view in most cases.
+        /// If set to <c>true<c/>, rotational patch is enabled and users can only look in positive z directions.
+        /// </summary>
+        [Tooltip("Workaround for wrong kinect rotations.")]
+        public bool forceForwardZ = true;
+
+        /// <summary>
+        /// Defines the eye of a stereo or mono output target.
+        /// </summary>
+        public enum StereoTarget
+        {
+            Mono,
+            Left,
+            Right
+        }
+
+        /// <summary>
+        /// Returns the eye position based on the <see cref="StereoTarget"/>.
+        /// </summary>
+        /// <param name="stereoTarget">The target eye (stereo) or the eye anchor (mono).</param>
+        /// <param name="seperation">The distance between the eyes (ignored when using <see cref="StereoTarget.Mono"/>).</param>
+        /// <returns>The eye position in world space.</returns>
+        public Vector3 GetPosition(StereoTarget stereoTarget, float seperation = 0f)
+        {
+            float seperationMultiplier = seperation * 0.5f;
+
+            // This is a "patch" for the kinect detecting abnormal rotations when having the 3D glasses on.
+            if (forceForwardZ)
+            {
+                var euler = transform.rotation.eulerAngles;
+                var angleYAbs = Math.Abs(euler.y);
+                seperationMultiplier *= (angleYAbs < 90 || angleYAbs > 270) ? -1 : 1;
+            }
+
+            switch (stereoTarget)
+            {
+                case StereoTarget.Mono:
+                    return transform.position;
+                case StereoTarget.Left:
+                    return transform.TransformPoint(Vector3.right * -seperationMultiplier);
+                case StereoTarget.Right:
+                    return transform.TransformPoint(Vector3.right * seperationMultiplier);
+            }
+
+            return Vector3.zero;
+        }
+    }
 }
